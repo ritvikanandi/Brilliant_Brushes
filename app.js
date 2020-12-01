30a987947955ff6dbed661c41eaf118c99b3ffa1
ritvika <ritvika30@gmail.com>
2020-12-01 09:51:07 +0530

    Added shop skeleton

---
+++ app.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
+
+app.listen(3000);
