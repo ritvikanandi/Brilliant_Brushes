6139ed5a7732789184dc224d7ab9952ebb77fcb5
ritvika <ritvika30@gmail.com>
2020-12-02 09:51:07 +0530

    Restructured shop

--- app.js
+++ app.js
@@ -8,14 +8,13 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
-app.use("/admin", adminData.routes);
-app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+app.use("/", shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
